0e2b46aec52eaac4698371ed9f81c788e2abfbe5
Alex <34450267+sawapont@users.noreply.github.com>
2022-04-30 01:46:21 +0300

    Add project files.

---
+++ CssIsolationViaRenderTreeBuilder/Shared/MyComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace CssIsolationViaRenderTreeBuilder.Shared
+{
+    public class MyComponent : ComponentBase
+    {
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+
+            builder.OpenElement(1, "div");
+            builder.AddAttribute(2, "class", "my-class");
+            builder.AddContent(3, "My component 1");
+
+            builder.CloseElement();
+        }
+    }
+}
